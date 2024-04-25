36d0a071fefb196db7ddd06c97d8731217e826c6
zhacyn <tsynduev@mail.ru>
2024-04-25 21:10:52 +0300

    Practika - 1

---
+++ scripts/practice_1/main.py
@@ -0,0 +1,26 @@
+# import sys
+# from PySide6.QtWidgets import QApplication, QWidget
+from PySide6 import QtWidgets
+
+
+# app = QApplication(sys.argv)
+#
+# window = QWidget()
+# window.setWindowTitle('Простейшее окно')
+# window.show()
+#
+# sys.exit(app.exec())
+
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Основное окно')
+        self.resize(300, 200)
+
+
+app = QtWidgets.QApplication()
+
+window = MainWindow()
+window.show()
+
+app.exec()
