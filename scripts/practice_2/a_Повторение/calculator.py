aa7c2f7e566a176a8b01c2885d1d297cff2e6adc
zhacyn <tsynduev@mail.ru>
2024-05-08 21:37:40 +0300

    Practika - 2

--- scripts/practice_2/a_Повторение/calculator.py
+++ scripts/practice_2/a_Повторение/calculator.py
@@ -10,24 +10,20 @@ class Window(QtWidgets.QWidget):
         self.ui.setupUi(self)
 
 
-        self.ui.pushButton_2.clicked.connect(self.mirror)
-        self.ui.pushButton_3.clicked.connect(self.mirror)
-        self.ui.multiplication.clicked.connect(self.mirror)
-        self.ui.division.clicked.connect(self.mirror)
+        self.ui.pushButton_2.clicked.connect(lambda: self.mirror("2"))
+        # self.ui.pushButton_3.clicked.connect(self.mirror)
 
-        self.ui.pushButton_C.clicked.connect(self.mirror)
-        self.ui.answer.clicked.connect(self.mirror)
+    def mirror(self, number):
+        self.ui.lineEdit.setText(self.ui.pushButton_2.text()+number)
 
 
-    def mirror(self):
-        text_2 = self.ui.pushButton_2.text()
-        text_3 = self.ui.pushButton_3.text()
-        text_multiplication = self.ui.multiplication.text()
-        text_division = self.ui.division.text()
-        self.ui.lineEdit.setText(text_2, text_3, text_multiplication, text_division)
 
-        text_C = (self.ui.pushButton_2.text("2"))
-        text_answer = (self.ui.pushButton_2.text("2"))
+
+        # self.ui.multiplication.clicked.connect(self.mirror) + number
+        # self.ui.division.clicked.connect(self.mirror)
+        #
+        # self.ui.pushButton_C.clicked.connect(self.mirror)
+        # self.ui.answer.clicked.connect(self.mirror)
 
 
 
